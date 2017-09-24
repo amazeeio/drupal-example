5c64417c0c9fce851748159438bc79ce0a18b704
Schnitzel <michael@amazee.com>
2017-09-24 10:10:46 +0200

    using config installer, exporting config

--- Dockerfile.builder
+++ Dockerfile.builder
@@ -1,4 +1,4 @@
-FROM amazeeio/centos7-php7.0-drupal-builder
+FROM amazeeiolagoon/centos7-php7.0-drupal-builder:develop
 
 COPY composer.json composer.lock /app/
 COPY scripts /app/scripts
--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -112,4 +112,4 @@ if (file_exists(__DIR__ . '/settings.local.php')) {
 if (file_exists(__DIR__ . '/services.local.yml')) {
   $settings['container_yamls'][] = __DIR__ . '/services.local.yml';
 }
-$settings['install_profile'] = 'standard';
+$settings['install_profile'] = 'config_installer';
