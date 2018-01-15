251ec26d1cc874215dce281904eb63442dcaaaa9
Schnitzel <michael@amazee.com>
2018-01-15 22:47:20 +0100

    encourage people to use `drush si config_installer` via drush policy. Also set `install_profile` to `config_installer` already

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -126,3 +126,5 @@ if (file_exists(__DIR__ . '/settings.local.php')) {
 if (file_exists(__DIR__ . '/services.local.yml')) {
   $settings['container_yamls'][] = __DIR__ . '/services.local.yml';
 }
+
+$settings['install_profile'] = 'config_installer';
