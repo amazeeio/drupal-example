e884557425426917df1fe57da5b23d5f03d13436
Schnitzel <michael@amazee.com>
2018-03-23 15:12:36 -0500

    better redis: - allow to use it even if the module is not enabled - use it for lock, flood, cache_tags (via including example.services.yml)

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -60,11 +60,52 @@ if (getenv('LAGOON')){
   $settings['redis.connection']['host'] = getenv('REDIS_HOST') ?: 'redis';
   $settings['redis.connection']['port'] = 6379;
 
-  // HINT: Uncomment in order to enable Redis
-  // # Do not set the cache during installations of Drupal
-  // if (!drupal_installation_attempted()) {
-  //   $settings['cache']['default'] = 'cache.backend.redis';
-  // }
+  # Do not set the cache during installations of Drupal
+  if (!drupal_installation_attempted() && extension_loaded('redis')) {
+    $settings['cache']['default'] = 'cache.backend.redis';
+
+    // Include the default example.services.yml from the module, which will
+    // replace all supported backend services (that currently includes the cache tags
+    // checksum service and the lock backends, check the file for the current list)
+    $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
+
+    // Allow the services to work before the Redis module itself is enabled.
+    $settings['container_yamls'][] = 'modules/contrib/redis/redis.services.yml';
+
+    // Manually add the classloader path, this is required for the container cache bin definition below
+    // and allows to use it without the redis module being enabled.
+    $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
+
+    // Use redis for container cache.
+    // The container cache is used to load the container definition itself, and
+    // thus any configuration stored in the container itself is not available
+    // yet. These lines force the container cache to use Redis rather than the
+    // default SQL cache.
+    $settings['bootstrap_container_definition'] = [
+      'parameters' => [],
+      'services' => [
+        'redis.factory' => [
+          'class' => 'Drupal\redis\ClientFactory',
+        ],
+        'cache.backend.redis' => [
+          'class' => 'Drupal\redis\Cache\CacheBackendFactory',
+          'arguments' => ['@redis.factory', '@cache_tags_provider.container', '@serialization.phpserialize'],
+        ],
+        'cache.container' => [
+          'class' => '\Drupal\redis\Cache\PhpRedis',
+          'factory' => ['@cache.backend.redis', 'get'],
+          'arguments' => ['container'],
+        ],
+        'cache_tags_provider.container' => [
+          'class' => 'Drupal\redis\Cache\RedisCacheTagsChecksum',
+          'arguments' => ['@redis.factory'],
+        ],
+        'serialization.phpserialize' => [
+          'class' => 'Drupal\Component\Serialization\PhpSerialize',
+        ],
+      ],
+    ];
+  }
 }
 
 ### Lagoon Varnish & Reverse proxy settings
